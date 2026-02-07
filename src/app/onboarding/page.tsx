5b4f77fd27808034d41946694f66d42e8bc6d1d6
Baramee Thunyapoo <baramee1407@gmail.com>
2026-02-07 02:52:45 +0000

    datepicker cannot choose year

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -226,6 +225,9 @@ export default function OnboardingPage() {
                                         mode="single"
                                         selected={field.value}
                                         onSelect={field.onChange}
+                                        captionLayout="dropdown-nav"
+                                        fromYear={new Date().getFullYear() - 100}
+                                        toYear={new Date().getFullYear()}
                                         disabled={(date) =>
                                         date > new Date() || date < new Date("1900-01-01")
                                         }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -315,6 +315,9 @@ export default function ProfilePage() {
                                 mode="single"
                                 selected={field.value}
                                 onSelect={field.onChange}
+                                captionLayout="dropdown-nav"
+                                fromYear={new Date().getFullYear() - 100}
+                                toYear={new Date().getFullYear()}
                                 disabled={(date) =>
                                 date > new Date() || date < new Date("1900-01-01")
                                 }
